bb53c25445eea8dcd3bc1026e22552f3e04c9031
John Penney <john.penney@gmail.com>
2022-01-29 11:08:23 +0000

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,45 @@
+# Create Security Group to access web
+resource "azurerm_network_security_group" "web-vm-nsg" {
+  depends_on=[azurerm_resource_group.rg]
+
+  name                = "web-tier-nsg"
+  location            = "ukwest"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowWEB"
+    description                = "Allow web"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowSSH"
+    description                = "Allow SSH"
+    priority                   = 150
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Associate the web NSG with the subnet
+resource "azurerm_subnet_network_security_group_association" "web-vm-nsg-association" {
+  depends_on=[azurerm_resource_group.rg]
+
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.web-vm-nsg.id
+}
