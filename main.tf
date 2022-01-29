a9b909c4103660556fb1e773a7f2b2d8e774606f
John Penney <john.penney@gmail.com>
2022-01-29 11:06:38 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,28 @@
-# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.65"
+      version = ">= 2.26"
     }
   }
-
-  required_version = ">= 0.14.7"
+ required_version = ">= 0.14.9"
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "tfstatevzzap"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
 }
 
+
+# Create a resource group for network
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+  name     = "UK-Linux-Team-rg"
   location = "ukwest"
-   tags = {
-     Environment = "Terraform Getting Started"
-     Team = "DevOps"
-   }
 }
 
-# Create a virtual network
-resource "azurerm_virtual_network" "vnet" {
-    name                = "myTFVnet"
-    address_space       = ["10.1.0.0/16"]
-    location            = "ukwest"
-    resource_group_name = azurerm_resource_group.rg.name
-}
