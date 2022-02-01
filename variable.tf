c7c5d4bdfca09e7774b81094f95f60f186daa2d6
John Penney <john.penney@gmail.com>
2022-02-01 08:34:21 +0000

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,30 @@
+variable "resource_group_name" {
+  default = "Unix-Team-RG"
+}
+
+variable "location" {
+  default = "ukwest"
+}
+
+variable "virtual_network" {
+  default = "web-tier"  
+}
+variable "public_ip" {
+  default = "PublicIP"
+}
+variable "subnet_name" {
+  default = "web-tier-sn"
+}
+
+variable "Network_interface_name" {
+  default = "web-nic"
+}
+
+variable "Network_security_group_name" {
+  default = "web-tier-nsg"
+}
+
+variable "Tags_enviroment" {
+  type = string
+  default = "Dev Terraform Build"
+}
