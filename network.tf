61e122ff782b93e027f7a0bfd45dceddbf9b5224
John Penney <john.penney@gmail.com>
2022-02-02 08:34:06 +0000

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
-  depends_on=[azurerm_resource_group.rg]
+  depends_on          = [azurerm_resource_group.rg]
   name                = var.virtual_network
   address_space       = ["10.0.0.0/16"]
   location            = var.location
@@ -10,39 +10,39 @@ resource "azurerm_virtual_network" "vnet" {
 
 # Create public IPs
 resource "azurerm_public_ip" "publicip" {
-    name                         = var.public_ip
-    location                     = var.location
-    resource_group_name          = azurerm_resource_group.rg.name
-    allocation_method            = "Dynamic"
+  name                = var.public_ip
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
 
-    tags = {
-        environment = var.Tags_enviroment
-    }
+  tags = {
+    environment = var.Tags_enviroment
+  }
 }
 data "azurerm_public_ip" "gateway-public-ip" {
-    name                        = "${azurerm_public_ip.publicip.name}"
-    resource_group_name = azurerm_resource_group.rg.name
+  name                = azurerm_public_ip.publicip.name
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 output "ServerExternalIP" {
-   value = "${data.azurerm_public_ip.gateway-public-ip.ip_address}"
+  value = data.azurerm_public_ip.gateway-public-ip.ip_address
 }
 
 # Create subnet
 resource "azurerm_subnet" "subnet" {
-    name                 = var.subnet_name
-    resource_group_name  = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes       = ["10.0.1.0/24"]
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "web-private-nic" {
-  depends_on=[azurerm_resource_group.rg]
+  depends_on = [azurerm_resource_group.rg]
 
   name                = var.Network_interface_name
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  
+
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
