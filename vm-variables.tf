70ee69bdeb33770ac089ef05510a6678936568c9
John Penney <john.penney@gmail.com>
2022-02-02 08:32:38 +0000

    Update vm-variables.tf

--- vm-variables.tf
+++ vm-variables.tf
@@ -1,15 +1,15 @@
 variable "web_vm_size" {
-  default = "Standard_B1s"  
+  default = "Standard_B1s"
 }
 variable "web_vm_username" {
   default = "azureuser"
 }
 
 variable "web_hostname" {
-  default = "webserver1.local"  
+  default = "webserver1.local"
 }
 variable "vm_web_name" {
-  default ="RHEL8_web"
+  default = "RHEL8_web"
 }
 #variable "web_vm_image" {
 #  type        = map(string)
@@ -28,10 +28,10 @@ variable "vm_web_name" {
 variable "web_vm_image" {
   type        = map(string)
   description = "Virtual machine source image information"
-  default     = {
-  publisher = "RedHat"
-  offer     = "RHEL"
-  sku       = "8-lvm-gen2" 
-  version   = "latest"
+  default = {
+    publisher = "RedHat"
+    offer     = "RHEL"
+    sku       = "8-lvm-gen2"
+    version   = "latest"
   }
 }
